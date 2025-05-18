e461bb7f1da12566573e426ac1fd148ad3aa6aa8
Galileo Kim <galileokim@Galileos-MacBook-Pro.local>
2025-05-18 01:36:41 -0600

    exercise 3.7-3.8

--- pt3/nodephonebook/index.js
+++ pt3/nodephonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
+morgan.token('data', (request, response) => JSON.stringify(request.body))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"));
+  
 
 let persons = [
     { 
